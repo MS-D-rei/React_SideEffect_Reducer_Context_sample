d82d34c3f08b0aed68558a7c169b49d511d4e973
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-10-18 22:22:00 +0900

    add useReducer for email and its validation

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import {
   StyledCardLogin,
   StyledLabel,
@@ -7,6 +7,37 @@ import {
   StyledAlignCenter,
 } from '@/components/Login/Style';
 import { StyledButton } from '@/components/UI/Button/StyledButton';
+
+interface ReducerEmailState {
+  value: string;
+  isValid: boolean;
+}
+
+enum ReducerEmailActionTypes {
+  USER_INPUT = 'USER_INPUT',
+  INPUT_BLUR = 'INPUT_BLUR',
+}
+
+interface ReducerEmailAction {
+  type: ReducerEmailActionTypes;
+  payload: string;
+}
+
+const emailReducer = (state: ReducerEmailState, action: ReducerEmailAction) => {
+  if (action.type === 'USER_INPUT') {
+    return {
+      value: action.payload,
+      isValid: !!action.payload.match(emailRegEx),
+    };
+  }
+  if (action.type === 'INPUT_BLUR') {
+    return { value: state.value, isValid: !!state.value.match(emailRegEx) };
+  }
+  return { value: '', isValid: false };
+};
+
+const emailRegEx =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 function Login(props: { onLogin: Function }) {
   const [emailPassword, setEmailPassword] = useState({
@@ -17,6 +48,11 @@ function Login(props: { onLogin: Function }) {
   const [isValidPassword, setIsValidPassword] = useState(false);
   const [isValidForm, setIsValidForm] = useState(false);
 
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: false,
+  });
+
   useEffect(() => {
     const identifier = setTimeout(() => {
       setIsValidForm(
@@ -26,21 +62,21 @@ function Login(props: { onLogin: Function }) {
       console.log('setTimeout works');
     }, 500);
 
-    
     return () => {
       clearTimeout(identifier);
       console.log('Clean up');
     };
   }, [emailPassword]);
 
-  const emailRegEx =
-    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-
   const emailChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmailPassword((prevState) => ({
-      ...prevState,
-      email: event.target.value,
-    }));
+    // setEmailPassword((prevState) => ({
+    //   ...prevState,
+    //   email: event.target.value,
+    // }));
+    dispatchEmail({
+      type: ReducerEmailActionTypes.USER_INPUT,
+      payload: event.target.value,
+    });
     // setIsValidForm(
     //   !!event.target.value.match(emailRegEx) &&
     //     emailPassword.password.trim().length >= 8
@@ -61,13 +97,15 @@ function Login(props: { onLogin: Function }) {
   };
 
   const validateEmailHandler = () => {
-    setIsValidEmail(!!emailPassword.email.match(emailRegEx));
-    // setIsValidForm(isValidEmail && isValidPassword);
+    dispatchEmail({
+      type: ReducerEmailActionTypes.INPUT_BLUR,
+      payload: '',
+    });
+    // setIsValidEmail(!!emailPassword.email.match(emailRegEx));
   };
 
   const validatePasswordHandler = () => {
     setIsValidPassword(emailPassword.password.trim().length >= 8);
-    // setIsValidForm(isValidEmail && isValidPassword);
   };
 
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
@@ -83,8 +121,8 @@ function Login(props: { onLogin: Function }) {
           <StyledInput
             type="email"
             id="email"
-            value={emailPassword.email}
-            isValid={isValidEmail}
+            value={emailState.value}
+            isValid={emailState.isValid}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
